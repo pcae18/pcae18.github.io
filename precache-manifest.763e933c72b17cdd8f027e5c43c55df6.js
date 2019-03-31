f76535404479473c9a8a6a88922fb84fd0f03032
Paulo Angeles <pcae18@gmail.com>
2019-03-31 13:06:50 -0600

    Aplicación de clima react

---
+++ precache-manifest.763e933c72b17cdd8f027e5c43c55df6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1b2618321c31dd2aaba6",
+    "url": "/static/js/main.c357b780.chunk.js"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "1b2618321c31dd2aaba6",
+    "url": "/static/css/main.73e0e076.chunk.css"
+  },
+  {
+    "revision": "5570984889c29b5f377da7195b9e92c2",
+    "url": "/index.html"
+  }
+];
